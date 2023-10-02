728a72fe421608aa0aefdd9ba5d1ad063f397667
Ana <anatran334@gmail.com>
2023-10-02 17:10:20 -0400

    create currency data class
    list string will contain drop down values
    updated main screen UI

---
+++ lib/currency_data.dart
@@ -0,0 +1,11 @@
+const List<String> currencyList = [
+  'CAD',
+  'USD',
+  'EUR',
+  'JPY',
+  'AUD',
+  'GBP',
+  'CNY',
+  'CHF',
+  'SGD'
+];
--- lib/screens/mainscreen.dart
+++ lib/screens/mainscreen.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
 
-class MainScreen extends StatefulWidget {
+class MainScreen extends StatelessWidget {
   const MainScreen({Key? key}) : super(key: key);
   static const id = "main_screen";
 
   @override
-  State<MainScreen> createState() => _MainScreenState();
-}
-
-class _MainScreenState extends State<MainScreen> {
-  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Convert'),
+      ),
+      body: SafeArea(
+        child: Row(
+          children: [
+            Column(
+              children: [],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
