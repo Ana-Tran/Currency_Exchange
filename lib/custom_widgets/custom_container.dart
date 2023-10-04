212629d7e4cf959b6c9ca4430753f13c2bd372f2
Ana <anatran334@gmail.com>
2023-10-04 16:10:15 -0400

    updated main screen
    updated UI of main screen
    created drop down menu in container
    moved custom container widget to new class

---
+++ lib/custom_widgets/custom_container.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+Container buildContainer(DropdownButton dropdownWidget) {
+  return Container(
+    alignment: Alignment.center,
+    padding: const EdgeInsets.all(10.0),
+    height: 100.0,
+    color: Colors.white,
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text('Select curreny you wish to convert'),
+        dropdownWidget,
+      ],
+    ),
+  );
+}
--- lib/screens/mainscreen.dart
+++ lib/screens/mainscreen.dart
@@ -1,23 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:currency_exchange/currency_data.dart';
+import 'package:currency_exchange/custom_widgets/custom_container.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
   static const id = "main_screen";
 
   @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  String selectedCurrency = 'USD';
+
+  DropdownButton createDropDown() {
+    List<DropdownMenuItem<String>> menuItems = [];
+    for (String currency in currencyList) {
+      var newItems = DropdownMenuItem(child: Text(currency), value: currency);
+      menuItems.add(newItems);
+    }
+    return DropdownButton<String>(
+        value: selectedCurrency,
+        items: menuItems,
+        onChanged: (value) {
+          setState(() {
+            selectedCurrency = value!;
+          });
+        });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('Convert'),
+        title: const Text('Convert'),
       ),
-      body: SafeArea(
-        child: Row(
-          children: [
-            Column(
-              children: [],
-            )
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        // crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          buildContainer(createDropDown()),
+        ],
       ),
     );
   }
